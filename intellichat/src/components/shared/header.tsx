4f968d8082892320c867fcc461d1e20872503f07
eytyy <e.tayyem@gmail.com>
2023-09-13 00:41:28 +0300

    clear messages on clicking logo

--- intellichat/src/components/shared/header.tsx
+++ intellichat/src/components/shared/header.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import React from 'react';
 import Logo from './logo';
 import Container from './container';
 import SideBar from './sidebar';
+import { Button } from '@/components/ui/button';
+import { useChatSettings } from '@/store/chat-settings';
 
 type Props = {};
 
 export default function Header({}: Props) {
+  const clearMessages = useChatSettings((s) => s.clearMessages);
   return (
     <header className='sticky top-0 z-[60] bg-background py-8 shadow-sm'>
       <Container className='flex max-w-full items-center justify-between'>
-        <div>
+        <Button role='button' variant='link' onClick={clearMessages}>
           <Logo />
-        </div>
+        </Button>
         <nav className='flex items-center gap-10'>
           <SideBar />
         </nav>
